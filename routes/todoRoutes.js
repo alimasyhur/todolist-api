03e5d5613510728ab13cdbacc90717cf779650f4
Ali Masyhur <jegrag4ever@gmail.com>
2021-10-30 22:43:42 +0700

    feat: put endpoint and response to request

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -4,21 +4,28 @@ const Todo = require("../models/Todo");
 router.get("/", (req, res) => {
     Todo.find((err, result) => {
         if(err) throw new Error(err);
-        console.log(result);
+        res.json(result);
     })
 })
 
 router.post("/", (req, res) => {
     Todo.create(req.body, (err, result) => {
         if(err) throw new Error(err);
-        console.log(result);
+        res.json(result);
+    })
+})
+
+router.put("/:id", (req, res) => {
+    Todo.findOneAndUpdate({ _id: req.params.id, }, req.body, { new: true }, (err, result) => {
+        if(err) throw new Error(err);
+        res.json(result);
     })
 })
 
 router.delete("/:id", (req, res) => {
     Todo.findOneAndDelete({ _id: req.params.id }, (err, result) => {
         if(err) throw new Error(err);
-        console.log(result);
+        res.end();
     })
 })
 
